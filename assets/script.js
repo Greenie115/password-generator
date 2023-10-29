07f7e364f0025966914ff80ef1f6cec066a6910f
Greenie115 <dggreen545@gmail.com>
2023-10-29 16:34:55 +0000

    Event listener and on lick linked

--- assets/script.js
+++ assets/script.js
@@ -143,7 +143,7 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(passwordLengthPrompt);
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
@@ -153,4 +153,3 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword);
 
 getPasswordOptions();
-console.log(generatePassword(passwordLengthPrompt))
